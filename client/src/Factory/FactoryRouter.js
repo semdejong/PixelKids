30c132ad655e665c1816f05a97003faf97059aac
semdejong <semdejo2003@gmail.com>
2022-08-11 19:05:50 +0200

    Added a navbar and sidebar

--- client/src/Factory/FactoryRouter.js
+++ client/src/Factory/FactoryRouter.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { Button } from "antd";
+import { Routes, Route } from "react-router-dom";
 
-import useAuthHook from "../Pages/Auth/AuthPages/hooks/useAuthHook";
+import { ObjectPage, ObjectTypePage, UserPage } from "./Pages";
+import useFactorySideBar from "./Hooks/useFactorySideBar";
+
+import SideBar from "../Components/SideBar";
 
 export default function FactoryRouter() {
-  const { logout } = useAuthHook();
+  const { topSideBar, bottomSideBar } = useFactorySideBar();
 
   return (
-    <Button type="primary" onClick={logout}>
-      Logout
-    </Button>
+    <div className="flex h-full">
+      {/* //render sidebar */}
+      <SideBar
+        topSideBarItems={topSideBar}
+        bottomSideBarItems={bottomSideBar}
+      />
+      <div className="p-4">
+        <Routes>
+          <Route path="objects" element={<ObjectPage />} />
+          <Route path="objectType" element={<ObjectTypePage />} />
+          <Route path="users" element={<UserPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
---
+++ client/src/Factory/Hooks/useFactorySideBar.js
@@ -0,0 +1,54 @@
+import { useNavigate } from "react-router-dom";
+
+export default function useFactorySideBar() {
+  const navigate = useNavigate();
+
+  const topSideBar = [
+    {
+      label: "Factory",
+      onClick: () => {
+        navigate("/factory");
+      },
+      icon: "fa-industry",
+      show: document.cookie.includes("isAuth"),
+    },
+    {
+      label: "Objects",
+      onClick: () => {
+        navigate("/factory/objects");
+      },
+      icon: "fa-database",
+      show: document.cookie.includes("isAuth"),
+    },
+    {
+      label: "Object Types",
+      onClick: () => {
+        navigate("/factory/objectType");
+      },
+      icon: "fa-cubes",
+      show: document.cookie.includes("isAuth"),
+    },
+    {
+      label: "Users",
+      onClick: () => {
+        navigate("/factory/users");
+      },
+      icon: "fa-users",
+      show: document.cookie.includes("isAuth"),
+    },
+  ];
+
+  const bottomSideBar = [
+    {
+      label: "Settings",
+      onClick: () => {},
+      icon: "fa-gears",
+      show: document.cookie.includes("isAuth"),
+    },
+  ];
+
+  return {
+    topSideBar,
+    bottomSideBar,
+  };
+}
